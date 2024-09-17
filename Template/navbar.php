137ae8f379285eb2bac104eab1ef5cb757b9c44f
MMphepo <164493503+MMphepo@users.noreply.github.com>
2024-09-17 09:51:34 +0200

    added search an profile icons

---
+++ Template/navbar.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Template\CSS\styles.css">
+</head>
+<body>
+<nav>
+    <div class="icon">SMC</div>
+    <div class="links">
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Legislation</li>
+            <li>Contact us</li>
+        </ul>
+    </div>
+    <div class="profile">icon</div>
+    <div class="search">search</div>
+</nav>    
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include('Template/navbar.php');
+?>
+<iframe width="822" height="462" src="https://www.youtube.com/embed/GGGDfciqyvw" title="Social Media, Social Life: Teens Reveal Their Experiences" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+<?php
+include('./Template/preview.php');
